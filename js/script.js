ed08f5169d24ee692b16d9fd10c87002509b871e
Bartosz Pluta <barpluta@o2.pl>
2018-04-23 11:49:38 +0200

    Add comments in script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+/*
+The expected result is a pop-up window with a text field. After entering the name, you should see another window with the Hello text displayed
+*/
+
+var name = prompt('Enter your name'); /declaring a variable named name/
+alert('Hello, ' + name);/the alert function displays the value in a window in the browser/
+console.log('Hello, ' + name); /the value will be displayed in the Console tab in the development tools/
+
